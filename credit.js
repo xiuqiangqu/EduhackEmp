5696e64e2c0fdfbe038ec06d6e99a9569dd1f3fc
Haoran2009 <haoranie@hotmail.com>
2022-08-28 12:35:41 -0700

    polished it up
    also added ziwen's frame into the top right corner of screen

---
+++ credit.js
@@ -0,0 +1,11 @@
+setInterval(creditUpdateTimer, 1000);
+
+function creditUpdateTimer() {
+    // total_learning_time = total_learning_time + Math.floor(60 * Math.random());
+    let reward = "";
+    for (let i = 60; i < total_learning_time; i = i + 60) {
+        reward += "&#127775";
+    }
+    document.getElementById("score").innerHTML = total_learning_time.toString();
+    document.getElementById("reward").innerHTML = reward.toString();
+}
